207167f88aa8a3bab752815740c48ccab49440c5
jaeyunim00 <ijy00120877@gmail.com>
2023-03-12 23:31:48 +0900

    animate --ing

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,21 +1,53 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
-  const id = useParams().id;
-
-  const getMovie = async () => {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const [detailMovie, setDetailMovie] = useState({});
+  const getDetail = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+
+    setMovie(json.data.movie);
+    setLoading(false);
   };
 
   useEffect(() => {
-    getMovie();
+    getDetail();
   }, []);
 
-  return <div>Details</div>;
+  console.log(movie);
+
+  return (
+    <div>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          <div className="movie_content">
+            <span>ja2yun000</span>
+            <h2>{movie.title}</h2>
+            <div className="info">
+              <span>{movie.rating}</span>
+              <span>{movie.year}</span>
+              <ul>
+                {movie.genres.map((item) => (
+                  <li>{item}</li>
+                ))}
+              </ul>
+            </div>
+            <div>{movie.description_full}</div>
+          </div>
+          <div className="movie_image">
+            <img src={movie.medium_cover_image} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
-
+import SwipApp from "../components/Swiper";
 const Home = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
@@ -20,21 +20,33 @@ const Home = () => {
     getMovies();
   }, []);
 
+  console.log(movies);
+
   return (
     <div>
+      <header>
+        <span class="material-symbols-outlined">
+          airline_seat_recline_extra
+        </span>
+        <h1>ja2yun000</h1>
+      </header>
       {loading ? (
         <div>Loading</div>
       ) : (
-        movies.map((movie) => (
-          <Movie
-            id={movie.id}
-            medium_cover_image={movie.medium_cover_image}
-            url={movie.url}
-            title={movie.title}
-            genres={movie.genres}
-            summary={movie.summary}
-          ></Movie>
-        ))
+        <div className="movies">
+          <div className="movies-wrap">
+            {movies.slice(0, 12).map((movie) => (
+              <Movie
+                id={movie.id}
+                medium_cover_image={movie.medium_cover_image}
+                url={movie.url}
+                title={movie.title}
+                genres={movie.genres}
+                summary={movie.summary}
+              ></Movie>
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );
